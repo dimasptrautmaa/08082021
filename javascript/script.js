f1b5012d5b8b0a33c676ce31ae725afb240d7639
dimasptrautmaa <dimasptraautmaa@gmail.com>
2023-03-12 18:32:04 +0700

    big update

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,23 @@
-const two = document.getElementById('two');
+const two = document.getElementById('two')
+const one = document.getElementById('one')
+const three = document.getElementById('three')
+const button = document.getElementById('button')
+const audio = document.getElementById('audio')
 
-two.addEventListener('click', function(){
-    
+two.addEventListener('mouseenter', function(){
+    two.classList.add('twoplus')
+    one.classList.add('gray')
+    three.classList.add('gray')
+    button.classList.add('show')
+})
+
+button.addEventListener('click', function(){
+    audio.play()
+})
+
+two.addEventListener('mouseleave', function(){
+    two.classList.remove('twoplus')
+    one.classList.remove('gray')
+    three.classList.remove('gray')
+    button.classList.remove('show')
 })
