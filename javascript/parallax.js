872c6f2ad9c5c7ba7fa29da6057f7fb1f86532f7
dimasptrautmaa <dimasptraautmaa@gmail.com>
2023-03-14 08:57:28 +0700

    update

--- javascript/parallax.js
+++ javascript/parallax.js
@@ -1,6 +1,8 @@
 const b1 = document.getElementById('b1')
 const b2 = document.getElementById('b2')
 const pilus = document.getElementById('pilus')
+const love = document.getElementById('love')
+const love2 = document.getElementById('love2')
 
 
 window.addEventListener('scroll', function(){
@@ -12,4 +14,11 @@ window.addEventListener('scroll', function(){
     b2.style.bottom = value * -0.45 + 'px'
     b2.style.right = value * -0.25 + 'px'
 
+    love.style.paddingTop = value * 0.5 + 'px'
+    love.style.rotate = value * 0.01 + 'deg'
+
+    love2.style.paddingTop = value * 0.2 + 'px'
+    love2.style.paddingLeft = value * 0.15 + 'px'
+    love2.style.rotate = value * 0.01 + 'deg'
+    love2.style.transformOrigin = 'left'
 })
